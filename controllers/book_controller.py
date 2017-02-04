e20ba281c58b4e3099695fd857c5fbf64dde6809
Allen Garvey <allen.garvey@gmail.com>
2017-02-03 19:16:02 -0500

    Refactor method in book controller to add a book and get a single book, and add functionality to get list of all books

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -4,19 +4,48 @@ import json
 from models.book import Book
 
 class BookController(webapp2.RequestHandler):
+    
+    #if book_id is given, returns data on specific book
+    #otherwise returns all books
     def get(self, book_id=None):
-    	if book_id:
-        	book = ndb.Key(urlsafe=book_id).get()
-        	self.response.write(book.to_json())
+    	#if a book id is sent, return the book
+        if book_id:
+            #if book not found will cause an error
+            try:
+                book = ndb.Key(urlsafe=book_id).get()
+                self.write_json(book.to_json())
+            except:
+                #error on not found
+                self.response.set_status(404)
+        #return list of all books
         else:
-        	self.response.write("hello from books")
+            books = map(lambda book: book.to_json_dict(), Book.all())
+            self.write_json(json.dumps(books))
 
     def post(self):
-    	book_parent_key = ndb.Key(Book, "book parent")
     	#parse json from request body
     	book_data = json.loads(self.request.body)
-    	book = Book(title=book_data['title'], isbn=book_data['isbn'], checked_in=False, parent=book_parent_key)
-    	#save in cloud datastore
+        #missing post body information will raise exception, so we need to use try
+        try:
+            book = Book(title=book_data['title'], isbn=book_data['isbn'], parent=Book.parent_key())
+        except:
+            #if creating the book failed, it's most likely due to missing data is post body
+            #so just return 'bad request' and exit
+            self.response.set_status(400)
+            return
+    	
+        #add optional keys, if they are sent in request body
+        for optional_key in ['checkedIn', 'genre']:
+            if optional_key in book_data:
+                setattr(book, optional_key, book_data[optional_key])
+
     	book.put()
+        #Set HTTP status code to 'created'
     	self.response.set_status(201)
-    	self.response.write(book.to_json())
+        #return json values of newly created book
+    	self.write_json(book.to_json())
+
+    #convenience method for writing json response
+    def write_json(self, json_string):
+        self.response.content_type = 'application/json'
+        self.response.write(json_string)
