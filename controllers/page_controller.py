6ec8055c1a37ff98804ba1120e2809cf622d1aac
Allen Garvey <allen.garvey@gmail.com>
2017-02-03 19:40:40 -0500

    Add route to delete all books and customers

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -1,5 +1,12 @@
 import webapp2
+from controllers.book_controller import BookController
 
 class PageController(webapp2.RequestHandler):
     def get(self):
         self.response.write("hello")
+
+    #delete all books and all customers
+    def delete(self):
+    	BookController.delete_all_books()
+    	#HTTP no content
+        self.response.set_status(204)
