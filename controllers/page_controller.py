8be5e315c070e709cd10d6cfca9b8b24426fff6f
Allen Garvey <allen.garvey@gmail.com>
2017-02-03 20:32:04 -0500

    Add functionality to delete all items in datastore to delete all customers

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -1,5 +1,6 @@
 import webapp2
 from controllers.book_controller import BookController
+from controllers.customer_controller import CustomerController
 
 class PageController(webapp2.RequestHandler):
     def get(self):
@@ -8,5 +9,6 @@ class PageController(webapp2.RequestHandler):
     #delete all books and all customers
     def delete(self):
     	BookController.delete_all_books()
+    	CustomerController.delete_all_customers()
     	#HTTP no content
         self.response.set_status(204)
