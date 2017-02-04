693f27a7568896cdd00f5c11ec82de8c98e4eacc
Allen Garvey <allen.garvey@gmail.com>
2017-02-03 19:15:16 -0500

    Add genre column to books and add methods for parent key and json dictionary

--- models/book.py
+++ models/book.py
@@ -2,12 +2,32 @@ from google.appengine.ext import ndb
 import json
 
 class Book(ndb.Model): 
-	"""Book"""
-	title = ndb.StringProperty()
-	isbn = ndb.StringProperty()
-	checked_in = ndb.BooleanProperty()
+	#parent key for books in google datastore
+	@classmethod
+	def parent_key(cls):
+		return ndb.Key(cls, "book parent")
 
-	def to_json(self):
+	#returns all books
+	@classmethod
+	def all(cls):
+		return cls.query(ancestor=cls.parent_key()).fetch()
+	
+	"""Book"""
+	title = ndb.StringProperty(required=True)
+	isbn = ndb.StringProperty(required=True)
+	#genre is an array of strings
+	genre = ndb.StringProperty(repeated=True)
+	#a book is by default checkedIn when it is created, unless otherwise set
+	checkedIn = ndb.BooleanProperty(default=True)
+
+
+	#returns serialized dictionary of object values
+	#with included key
+	def to_json_dict(self):
 		book_dict = self.to_dict()
 		book_dict['id'] = self.key.urlsafe()
-		return json.dumps(book_dict)
+		return book_dict
+
+	#returns json formatted string
+	def to_json(self):
+		return json.dumps(self.to_json_dict())
